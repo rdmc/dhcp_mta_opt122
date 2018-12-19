8a69e0d89acfd9b9fe078b11292bb002a9156455
rdmc <rdmc69@gmail.com>
2018-12-19 10:28:54 -0100

    te

--- d.c
+++ d.c
@@ -4,47 +4,69 @@
 #include <inttypes.h>
 
 
-// static uint8_t *opt_hybrid = "\x7a\x20\x03\x12\x03mps\007CABOTVA\003NET\000";
 static uint8_t *opt_gap = "pktc*"; 
 
+static uint8_t *opt_init = "\x7A\x20";
 
-static uint8_t *opt_basic = "\x7A\x20" 
-			     "\x03\x13\x00\004mps0\007CABOTVA\003NET\x00"
+static uint8_t *opt_basic =  "\x03\x13\x00\004mps0\007CABOTVA\003NET\x00"
 			     "\x06\x09\005BASIC\001\x31\x00";
 
-
-static uint8_t *opt_hybrid = "\x7A\x20" 
-			     "\x03\x12\x00\003mps\007CABOTVA\003NET\x00"
+static uint8_t *opt_hybrid = "\x03\x12\x00\003mps\007CABOTVA\003NET\x00"
 			     "\x06\x0a\006HYBRID\001\x32\x00";
 
+static uint8_t *opt_end = "!!!!!!!!!";
 
-int opt_len = 0x20 + 2 ;  // data len  + len( x7a, 0x20)
-int gap_len = 0x05;
+static uint8_t buf[100];
 
 
-int main (int argc, char** argv) {
+int init_len = 0x02;
+int opt_len  = 0x20;  
+int gap_len  = 0x05;
+int end_len  = 0x09;
 
-	//fprintf(stderr, "opt_hybrid len=%d\n", opt_hybrid_len);
-	
-	uint8_t *p; 
-	int i;
+// forward func declaration
+void write_opt(uint8_t *start, int len); 
+void mcopy(uint8_t *dst, const uint8_t *src, int len);
 
-	p = opt_gap;
-	for (i= 0; i < gap_len; i++) {
-		putchar(*p);
-		p++;
-	}
-	
-	p = opt_hybrid;
-	//p = opt_basic;
-	for (i= 0; i < opt_len; i++) {
-		putchar(*p);
-		p++;
-	}
-	
-	//printf(p);
-	
+int main (int argc, char** argv) 
+{
+
+	memcpy(buf, opt_basic, opt_len );
+
+	write_opt(opt_gap, gap_len);
+	write_opt(opt_init, init_len);
+	write_opt(buf, opt_len);
+	write_opt(opt_end, end_len);
+
+	//memcpy(buf, opt_hybrid, opt_len);
+	mcopy(buf, opt_hybrid, opt_len);
+
+	write_opt(opt_gap, gap_len);
+	write_opt(opt_init, init_len);
+	write_opt(buf, opt_len);
+	write_opt(opt_end, end_len);
+
 
 	return 0;
+}
+
+void write_opt(uint8_t *start, int len) 
+{
+	uint8_t *p;
+	int i;
+
+	p = start;
+	for (i = 0; i < len; i++, p++) 
+		putchar(*p);
+}
+
+
+void mcopy(uint8_t *dst, const uint8_t *src, int len)
+{
+	//uint8_t *p;
+	int i;
+
+	for (i = 0; i < len; i++)
+		dst[i] = src[i];
 }
 
--- e.c
+++ e.c
@@ -33,28 +33,6 @@
 #define MAX_DHCP_FILE_LENGTH             128
 #define MAX_DHCP_OPTIONS_LENGTH          312
 
-
-struct dhcpmessage
-{
-    uint8_t op;
-    uint8_t htype;
-    uint8_t hlen;
-    uint8_t hops;
-    uint32_t xid;
-    uint16_t secs;
-    uint16_t flags;
-    uint32_t ciaddr;
-    uint32_t yiaddr;
-    uint32_t siaddr;
-    uint32_t giaddr;
-    char chaddr[16];
-    char sname[64];
-    char file[128];
-    char magic[4];
-    char opt[3];
-} __attribute__((__packed__));
-
-
 #define DHCP_CHADDR_LEN (16)
 #define DHCP_SNAME_LEN  (64)
 #define DHCP_FILE_LEN   (128)
@@ -90,6 +68,7 @@ typedef struct dhcp_packet {
 
 #define ETHERNET_HARDWARE_ADDRESS            1     /* used in htype field of dhcp packet */
 #define ETHERNET_HARDWARE_ADDRESS_LENGTH     6     /* length of Ethernet hardware addresses */
+#define ETH_ALEN     			     6     /* length of Ethernet hardware addresses */
 
 unsigned char client_hardware_address[MAX_DHCP_CHADDR_LENGTH]="";
 unsigned int my_client_mac[MAX_DHCP_CHADDR_LENGTH];
@@ -105,55 +84,91 @@ oui_t thg540_oui[] =  { {"\x00\x18\x9b"},
 	   		{"\x00\x1e\x99"},
 			{"\x00\x25\xab"} };
 
-// NOTE: the above typedef struct is safer the the thg540 used in is_thg540 functiuon.
-// read some 3rd party code to decide the safest and more eficient option.
+typedef struct mac {
+         uint8_t     data[ETH_ALEN];
+} mac_t;
 
+mac_t mac_list[] = { 
+	{"\x00\x18\x9B\x4A\x1B\x31"},
+        {"\x00\x18\x9B\x4A\x1B\x3D"},
+   	{"\x00\x18\x9B\x4A\x1B\x49"},
+   	{"\x00\x18\x9B\x5D\xFE\x20"},
+   	{"\x00\x18\x9B\x87\x16\x09"},
+   	{"\x00\x18\x9B\x89\x81\x47"},
+   	{"\x00\x18\x9B\x89\xF5\xDE"},
+   	{"\x00\x18\x9B\x8A\x50\x8C"},
+   	{"\x00\x18\x9B\x8A\x6E\x1D"},
+  	{"\x00\x18\x9B\x8A\xAF\x7E"} 	};	
 
-// OPTION 122 
-uint8_t *opt122_basic = "\172\024\003\017mps0.cabotva.net\006\007BASIC.1";
-	//opt 122,len 28, sub-opt 3, len 15, "mps0.cabota.net", sup-opt 6, len 7, "BASIC.1"
+size_t mac_list_len = sizeof(mac_list)/sizeof(mac_t);
 
-uint8_t *opt122_hybrid = "\x7a\x1d\x03\x0emps.cabotva.net\x06\x08HYBRID.2";
-	//opt 122,len 28, sub-opt 3, len 14=x0f, "mps.cabota.net", sup-opt=6, len=8, "HYBRID.2"
+mac_t my_mac = {"\x00\x18\x9b\x4a\x1b\x99"};
+
 
 //memcpy(discover_packet.chaddr,client_hardware_address,ETHERNET_HARDWARE_ADDRESS_LENGTH);
 
-int8_t is_thg540(uint8_t *chaddr);
+
+// forward refs
+//int8_t is_thg540(uint8_t *chaddr);
+int8_t is_thg540(mac_t *m);
+int8_t maccmp(mac_t*  m1, mac_t* m2);
+int8_t mac_inlist(mac_t* m);
 
 
 int main(int argc, char** argv) {
 
-	//printf("opt1: %d\nopt2: %d\n", strlen(opt122_basic), strlen(opt122_hybrid));
-	//printf(opt122_basic);
-	//printf(opt122_hybrid);
-/*	
-	for (int i =0; i<3; i++) {
-	       printf("%2.2x%2.2x%2.2x\n", thg540_oui[i].data[0],
-			       		   thg540_oui[i].data[1],
-					   thg540_oui[i].data[2]);
-	}
-*/
-	uint8_t mac[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55}; 
-
-	if (is_thg540(mac) == TRUE) {
-		printf("mac is a THG540\n");
+	if (is_thg540(&my_mac) == TRUE) {
+		printf("mac is a THG540.\n");
 	} else {
-		printf("mac unknow\n");
+		printf("mac OUI unknow.\n");
 	}
 
+/*
+	printf("mac_list size: %d\n", mac_list_len);
+	int i;
+	for (i = 0; i < mac_list_len; i++)
+		printf("%d\n", maccmp(&my_mac, &mac_list[i]));
+*/
+
+	if (mac_inlist(&my_mac) == TRUE) {
+		printf("mac is in the list.\n");
+	} else {
+		printf("mac unknow.\n");
+	}
+
+
+	// bye, bye
 	return 0;
+}
+
+int8_t mac_inlist(mac_t* m)
+{
+	int i;
+	for (i = 0; i < mac_list_len; i++)
+		if (maccmp(m, &mac_list[i]) == 0)
+			return TRUE;
+	return FALSE;
+}
+
+int8_t maccmp(mac_t*  m1, mac_t* m2)
+{
+	if (!m1 || !m2) return -1;
+	return memcmp(m1->data, m2->data, ETH_ALEN);
 }
 
 
 
-int8_t is_thg540(uint8_t *chaddr) {
+int8_t is_thg540(mac_t *m)
+{
+	uint8_t *chaddr;
+	
+	if (!m) return FALSE;
+	chaddr = m->data;
 
 	//THG540 OUI TABLE
 	uint8_t *thg540[] =  { "\x00\x18\x9b",
 		   	       "\x00\x1e\x99",
-			       "\x00\x25\xab", 
-			       "\x00\x00\x11",
-			       "\x00\x11\x22" };
+			       "\x00\x25\xab" }; 
 
 	size_t thg540_len =  sizeof(thg540)/sizeof(thg540[0]);
 	
@@ -168,8 +183,6 @@ int8_t is_thg540(uint8_t *chaddr) {
 	}
 	return FALSE;
 }
-
-
 
 
 // int8_t is_is_
